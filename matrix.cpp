befe885e78efeedfa9d8a305286095af94fdeaf4
hansupark <soo9878@likelion.org>
2021-03-16 18:22:56 +0900

    0316 opencv study

--- matrix.cpp
+++ matrix.cpp
@@ -5,75 +5,157 @@ using namespace cv;
 //Matrix Pratice
 
 
-//int main()
-//{
-//	//Matrix 초기화
-//	//Mat a; //아무 것도 안 정해진 행렬
-//	//Mat b(5, 3, CV_8U, Scalar(64)); // 5 x 3 행렬 생성, 8bit unsigned char, 64로 초기화
-//	//std::cout << b << '\n';
-//	//b.create(6, 4, CV_8U); // b 자리에 새로운 크기의 행렬 선언, 전에 선언했던 크기와 같다면 메모리 해제 x
-//	//std::cout << b << '\n';
-//
-//	//Mat c(Size(300, 300), CV_8UC3, Scalar(0, 0, 255)); // 5x3 의 행렬, 모든 원소 red 값을 가지게 함.
-//	//
-//	//imshow("c", c);
-//	//waitKey();
-//
-//	//Mat d(Size(5, 3), CV_8U, Scalar(300));
-//	//std::cout << "Matrix d : " << d << '\n'; //모든 원소가 255로 재설정, 부호없는 8비트이기 때문에 300이상 저장 x
-//
-//	//short data[] = { 1, 2, 3, 4, 5, 6 };
-//	//Mat e(2, 3, CV_8S, data); //행렬에서 참조하는 데이터와 맞지 자료형을 사용시 잘못 참조 될 수 있음.
-//	//std::cout << "Matrix e : " << e << '\n';
-//
-//	//Mat matrix_ones = Mat::ones(Size(3, 3), CV_8U);
-//	//std::cout << "Matrix ones : " << matrix_ones << '\n';
-//	//Mat matrix_I_matrix = Mat::eye(Size(3, 3), CV_8U);
-//	//std::cout << "Matrix I matrix : " << matrix_I_matrix << '\n';
-//
-//	//Mat A, C;
-//
-//	//A = imread("./image/PHS.jpg", IMREAD_COLOR); //Allocate the image to the Matrix.
-//	////IMREAD_COLOR: convert image to the 3 Channel RGB
-//
-//	//Mat B(A); //Copy operator --> copy only header and pointer
-//
-//	//C = A; //Assigment Operator
-//	//
-//	//rectangle(A, Rect(30, 25, 350, 30), Scalar(0, 0, 255));
-//	//namedWindow("A");
-//
-//	//imshow("A", A);
-//	//waitKey();
-//
-//	//namedWindow("B");
-//
-//	//imshow("B", B);
-//	//waitKey();
-//
-//	//namedWindow("C");
-//
-//	//imshow("C", C);
-//	//waitKey();
-//	////A, B, C point to the same image. --> The assignment operator and the copy constructor only copies the header.
-//
-//	//Mat F = A.clone();
-//	//rectangle(F, Rect(30, 25, 350, 30), Scalar(0, 0, 255));
-//	//Mat G;
-//	//A.copyTo(G);
-//	//rectangle(A, Rect(30, 25, 350, 30), Scalar(0, 255, 255));
-//	//Mat H;
-//
-//	//hconcat(A, F, H); //hconcat --> concat horizontally
-//	//hconcat(H, G, H); //vconcat --> concat vertically
-//
-//	//imshow("H", H);
-//	///*imshow("A", A);
-//	//imshow("F", F);
-//	//imshow("G", G);*/
-//
-//	//waitKey();
-//
-//	////clone(), copyTo() method copy the matrix ifself.
-//
-//}
+int main()
+{
+	//Matrix 초기화
+	//Mat a; 
+	//Mat b(5, 3, CV_8U, Scalar(64)); // 5 x 3 행렬 생성, 8bit unsigned char, 64로 초기화
+	//std::cout << b << '\n';
+	//b.create(6, 4, CV_8U); // b 자리에 새로운 크기의 행렬 선언, 전에 선언했던 크기와 같다면 메모리 해제 x
+	//std::cout << b << '\n';
+
+	// 모든 원소 값이 1인 행렬 초기화
+	//Mat matrix_ones = Mat::ones(Size(3, 3), CV_8U);
+	//std::cout << "Matrix ones : " << matrix_ones << '\n';
+
+	// 단위 행렬 초기화
+	//Mat matrix_I_matrix = Mat::eye(Size(3, 3), CV_8U);
+	//std::cout << "Matrix I matrix : " << matrix_I_matrix << '\n';
+
+	// createTo method 초기화
+	//Mat mat;
+	//mat.create(Size(2, 2), CV_8SC1); // create method는 원소 값을 초기화 할 수 없음.
+	//
+	//// = operator나 setTo method로 초기화
+	//mat = Scalar(100);
+	//std::cout << mat << '\n';
+
+	//mat.setTo(Scalar(200));
+	//std::cout << mat << '\n';
+
+	//Matrix methods
+	//Mat truecolor(Size(3, 3), CV_8UC3, Scalar(230, 100, 0, 0)); //각 원소값의 차원(channel)이 3차원인 행렬을 선언.
+	//std::cout << "true color : " << truecolor << '\n';
+	//std::cout << "true color's depth : " << truecolor.depth() << '\n';
+	//std::cout << "true color's channel : " << truecolor.channels() << '\n';
+
+	//std::cout << '\n';
+
+	//Mat graycolor(Size(3, 3), CV_8UC1, Scalar(230, 0, 0, 0)); //각 원소값의 차원(channel)이 3차원인 행렬을 선언.
+	//std::cout << "gray color : " << graycolor << '\n';
+	//std::cout << "gray color's depth : " << graycolor.depth() << '\n';
+	//std::cout << "gray color's channel : " << graycolor.channels() << '\n';
+
+	//Matrix error case
+	//Mat d(Size(5, 3), CV_8U, Scalar(300));
+	//std::cout << "Matrix d : " << d << '\n'; //모든 원소가 255로 재설정, 부호없는 8비트이기 때문에 300이상 저장 x
+
+	//short data[] = { 1, 2, 3, 4, 5, 6 };
+	//Mat e(2, 3, CV_8S, data); //행렬에서 참조하는 데이터와 맞지 자료형을 사용시 잘못 참조 될 수 있음.
+	////short는 2byte임으로 CV_16S여야 정상적으로 동작.
+	//std::cout << "Matrix e : " << e << '\n';
+
+
+	//Matrix copy
+
+	//image read
+	//Mat A, C;
+	//A = imread("./image/PHS.jpg", IMREAD_COLOR); //Allocate the image to the Matrix.
+	////IMREAD_COLOR: convert image to the 3 Channel RGB
+
+	// shallow copy
+	//Mat B(A); //Copy operator --> copy only header and pointer 
+
+	//C = A; //Assigment Operator
+	//
+	//rectangle(A, Rect(30, 25, 350, 30), Scalar(0, 0, 255));
+	//namedWindow("A");
+
+	//imshow("A", A);
+	//waitKey();
+
+	//namedWindow("B");
+
+	//imshow("B", B);
+	//waitKey();
+
+	//namedWindow("C");
+
+	//imshow("C", C);
+	//waitKey();
+	////A, B, C point to the same image. --> The assignment operator and the copy constructor only copies the header.
+
+	// deep copy
+
+	//clone, copyTo
+	//Mat F = A.clone();
+	//rectangle(F, Rect(30, 25, 350, 30), Scalar(0, 0, 255));
+	//Mat G;
+	//A.copyTo(G);
+	//rectangle(A, Rect(30, 25, 350, 30), Scalar(0, 255, 255));
+	//Mat H;
+
+	//hconcat(A, F, H); //hconcat --> concat horizontally
+	//hconcat(H, G, H); //vconcat --> concat vertically
+
+	//imshow("H", H);
+	///*imshow("A", A);
+	//imshow("F", F);
+	//imshow("G", G);*/
+
+	//waitKey();
+
+	////clone(), copyTo() method copy the matrix ifself.
+
+	//converTo --> data type을 변경시킴.
+	//Mat mat(Size(5, 5), CV_8UC1, Scalar(300));
+	//std::cout << mat << '\n';
+	//std::cout << '\n';
+
+	//mat.convertTo(mat, CV_16UC1); //8비트 -> 16비트
+	//mat = Scalar(300);
+	//std::cout << mat << '\n';
+	//std::cout << '\n';
+
+	//reshape, resize
+	//Mat mat(Size(5, 5), CV_8UC1, Scalar(5)); 
+	//Mat mat2;
+
+	////resize --> 행을 기준으로 resize시킴, 즉 행만 변경됨 // 기존 행렬에 수정
+	//std::cout << mat << '\n';
+	//std::cout << '\n';
+	//mat.resize(10, Scalar(10)); // 행을 10으로 늘리고 빈곳은 10으로 채움.
+	//std::cout << mat << '\n';
+	//std::cout << '\n';
+	////reshape
+
+	//mat2 = mat.reshape(25, 2); // 기존의 행렬에서 reshape한 상태의 새 행렬을 return 
+	//std::cout << mat << '\n';
+	//std::cout << mat2 << '\n';
+
+	//at
+	//Mat mat(Size(3, 3),CV_8UC1, Scalar(300));
+	//std::cout << mat << '\n';
+	//std::cout << '\n';
+	//for (int x = 0; x < mat.rows; x++) {
+	//	for (int y = 0; y < mat.cols; y++) {
+	//		//mat.at<uchar>(x, y) = 50; //정상 동작x
+	//		mat.at<uchar>(x, y) = 50; //template은 matrix의 data type과 맞춰야함.
+	//	}
+	//}
+	//std::cout << mat << '\n';
+	//std::cout << '\n';
+
+	//행렬 연산 함수
+	//short data[] = { 1,2,3,4,5,6,7,8,9 };
+	//Mat mat1(Size(3, 3), CV_16SC1, data);
+	//short data2[] = { 1,1,1,1,1,1,1,1,1 };
+	//Mat mat2(Size(3, 3), CV_16SC1, data2);
+
+	////std::cout << "mat1 and mat2's cross : " << mat1.cross(mat2) << '\n';
+	//std::cout << "mat1 and mat2's dot : " << mat1.dot(mat2) << '\n';
+	//std::cout << "mat1 and mat2's mul : " << mat1.mul(mat2) << '\n';
+
+	//std::cout << "mat1's inverse : " << mat1.inv() << '\n';
+	//std::cout << "mat1's transpose : " << mat1.t() << '\n';
+}
--- opencv_Base_Class.cpp
+++ opencv_Base_Class.cpp
@@ -3,105 +3,113 @@
 
 using namespace cv;
 
-int main()
-{
-
-	// Point_ 클래스 --> 가로와 세로 위치를 2차원 좌표로 나타내는 클래스
-	//Point_<int> p1(3, 4);
-	//Point_<int> p2(4, 5);
-
-	//int rs_inner = p1.dot(p2); //p1와 p2의 내적 값을 구함.
-	//std::cout << "p1와 p2의 내적 값 : " << rs_inner << '\n';
-	//
-	//double rs_cross = p1.cross(p2); //p1와 p2의 외적 값을 구함, 외적 : 두벡터와 직각인 벡터를 구함.
-	//std::cout << "p1와 p2의 외적 값 " << rs_cross << "\n";
-
-	////Point 연산 방식은 element-wise 방식
-	//Point_<int> p3 = p1 + p2; // (7, 9)
-	//Point_<int> p4 = p1 * 3; //  (9, 12)
-	//bool isSame = p1 == p2; //false
-
-	//std::cout << "p3 : " << p3 << '\n';
-	//std::cout << "p4 : " << p4 << '\n';
-	//std::cout << "isSame : " << isSame << '\n';
-
-	//Size_ 클래스 --> 이미지나 사각형 크기를 규정
-	//Size_<int> s1(4, 5); //(width, height)
-	//std::cout << "s1's (width, height) : " << s1 << '\n';
-	//std::cout << "s1's area : " << s1.area() << '\n'; // 20
-
-	//Size_<int> s2;
-	//s2.width = 5, s2.height = 6;
-	//std::cout << "s2's (width, height) : " << s2 << '\n';
-	//std::cout << "s2's area : " << s2.area() << '\n'; // 30
-
-	//Rect_ 클래스 --> 사각형을 나타내는 클래스
-	//Rect_<int> r1(3, 4, 5, 6); //Top_left 좌표가 (3, 4)이고 width x height 가 5 x 6인 직사각형
-	//Rect_<int> r2(r1); //복사 생성자
-	//Rect_<int> r3(Point_<int>(3, 4), Size_<int>(5, 6));
-	//Rect_<int> r4(Point_<int>(3, 4), Point_<int>(8, 10)); //Top_Left, Bottom_Right
-
-	//std::cout << "r1 == r2 : " << (r1 == r2) << '\n'; //element자체를 검사 --> 주소 검사 x
-	//std::cout << "r1 == r3 : " << (r1 == r3) << '\n';
-	//std::cout << "r1 == r4 : " << (r1 == r4) << '\n';
-
-	//std::cout << "r1's topleft and bottomright : " << r1.tl() << ", " << r1.br() << '\n';
-	//std::cout << "r1's size : " << r1.size() << '\n'; //rect의 size 정보를 Size_ class 형태로 return
-	//std::cout << "r1's area : " << r1.area() << '\n';
-
-	//Rect_<int> r5 = r1 + Point_<int>(1, 2); //r1를 (1,2)만큼 평행 이동
-	//Rect_<int> r6 = r1 + Size_<int>(2, 2); //r1의 size를 width:2, height:2 만큼 증가시킴
-	//std::cout << "r5's topleft and bottomright : " << r5.tl() << ", " << r5.br() << '\n';
-	//std::cout << "r6's size : " << r6.size() << '\n'; 
-
-	//Rect_<int> r7 = r5 & r6; //r5와 r6의 교차영역을 구함
-	//std::cout << "r7's topleft and bottomright : " << r7.tl() << ", " << r7.br() << '\n';
-	//std::cout << "r7's size : " << r7.size() << '\n';
-
-	//Rect_<int> r8 = r5 | r6; //r5와 r6의 합영역을 구함
-	//std::cout << "r8's topleft and bottomright : " << r8.tl() << ", " << r8.br() << '\n';
-	//std::cout << "r8's size : " << r8.size() << '\n';
-
-	//std::cout << "r8 contains Point(6,7)? : " << r8.contains(Point_<int>(6, 7)) << '\n'; //해당 point가 rect안에 포함되는지를 검사 
-
-	//Rotated Rect_ 클래스 --> 회전된 사각형 클래스
-	//중심좌표, size, angle로 사각형 조작
-
-	//RotatedRect rr1(Point2f(4.0, 5.0), Size2f(5.0, 6.0), 60); //(4.0, 5.0) 좌표가 중심인 크기가 5 x 6인 직사각형을 60도 회전시킴
-	//RotatedRect rr2(Point2f(0.0, 0.0), Point2f(1.0, 0.0), Point2f(1.0, 2.0)); //세 좌표로 직사각형 설정 --> 세좌표가 직사각형이여아함
-
-	//std::cout << "rr2's center, size, angle : " << rr2.center << ", " << rr2.size << ", " << rr2.angle << '\n';
-
-	//Point2f pts[4];
-	//rr2.points(pts); //pts에 rr2의 좌표 값 저장
-	//std::cout << "rr2's points" << '\n';
-
-	//for (Point2f p : pts) {
-	//	std::cout << p << '\n';
-	//}
-
-	//std::cout << rr1.boundingRect() << '\n'; //rr1를 포함하면서 최소 크기인 직사각형을 return
-
-	//Vec 클래스 --> 원소 수가 작을 때 사용하는 숫자 벡터 클래스
-	//Vec<int, 4> v1(1,2,3,4);
-	//std::cout << "v1 : " << v1 << '\n';
-
-	//Vec2f v2(3.1, 4.1);
-	//std::cout << "v2 : " << v2 << '\n';
-
-	//Point2f temp_p(v2); // vector --> point로 변환 가능
-	//std::cout << "temp_p : " << temp_p << '\n';
-
-	//Scalar 클래스 --> Vec<typename, 4>, 화소 값을 저장할때 많이 사용, Scaler_<double>의 약자.
-	Scalar_<float> bgr_t(0, 0, 255, 1);
-	std::cout << "bgr_t : " << bgr_t << '\n';
-	
-	Scalar gray_scale = 210; // 첫번째 원소값만 설정되고 나머지는 다 0으로 저장
-	std::cout << "gray_scale : " << gray_scale << '\n';
-	//Range 클래스 --> 시퀀스에서 연속되는 서브 시퀀스를 지정하는 클래스
-
-	//Range r(1, 4); // 1, 2, 3 --> end는 포함하지 않음.
-	//std::cout << "Range r : " << r << '\n';
-	//std::cout << "Range r's size : " << r.size() << '\n';
-	//std::cout << "Range r is empty ?  : " << r.empty() << '\n';
-}
+//int main()
+//{
+//
+//	 Point_ 클래스 --> 가로와 세로 위치를 2차원 좌표로 나타내는 클래스
+//	Point_<int> p1(3, 4);
+//	Point_<int> p2(4, 5);
+//
+//	int rs_inner = p1.dot(p2); //p1와 p2의 내적 값을 구함.
+//	std::cout << "p1와 p2의 내적 값 : " << rs_inner << '\n';
+//	
+//	double rs_cross = p1.cross(p2); //p1와 p2의 외적 값을 구함, 외적 : 두벡터와 직각인 벡터를 구함.
+//	std::cout << "p1와 p2의 외적 값 " << rs_cross << "\n";
+//
+//	//Point 연산 방식은 element-wise 방식
+//	Point_<int> p3 = p1 + p2; // (7, 9)
+//	Point_<int> p4 = p1 * 3; //  (9, 12)
+//	bool isSame = p1 == p2; //false
+//
+//	std::cout << "p3 : " << p3 << '\n';
+//	std::cout << "p4 : " << p4 << '\n';
+//	std::cout << "isSame : " << isSame << '\n';
+//
+//	Size_ 클래스 --> 이미지나 사각형 크기를 규정
+//	Size_<int> s1(4, 5); //(width, height)
+//	std::cout << "s1's (width, height) : " << s1 << '\n';
+//	std::cout << "s1's area : " << s1.area() << '\n'; // 20
+//
+//	Size_<int> s2;
+//	s2.width = 5, s2.height = 6;
+//	std::cout << "s2's (width, height) : " << s2 << '\n';
+//	std::cout << "s2's area : " << s2.area() << '\n'; // 30
+//
+//	Rect_ 클래스 --> 사각형을 나타내는 클래스
+//	Rect_<int> r1(3, 4, 5, 6); //Top_left 좌표가 (3, 4)이고 width x height 가 5 x 6인 직사각형
+//	Rect_<int> r2(r1); //복사 생성자
+//	Rect_<int> r3(Point_<int>(3, 4), Size_<int>(5, 6));
+//	Rect_<int> r4(Point_<int>(3, 4), Point_<int>(8, 10)); //Top_Left, Bottom_Right
+//
+//	std::cout << "r1 == r2 : " << (r1 == r2) << '\n'; //element자체를 검사 --> 주소 검사 x
+//	std::cout << "r1 == r3 : " << (r1 == r3) << '\n';
+//	std::cout << "r1 == r4 : " << (r1 == r4) << '\n';
+//
+//	std::cout << "r1's topleft and bottomright : " << r1.tl() << ", " << r1.br() << '\n';
+//	std::cout << "r1's size : " << r1.size() << '\n'; //rect의 size 정보를 Size_ class 형태로 return
+//	std::cout << "r1's area : " << r1.area() << '\n';
+//
+//	Rect_<int> r5 = r1 + Point_<int>(1, 2); //r1를 (1,2)만큼 평행 이동
+//	Rect_<int> r6 = r1 + Size_<int>(2, 2); //r1의 size를 width:2, height:2 만큼 증가시킴
+//	std::cout << "r5's topleft and bottomright : " << r5.tl() << ", " << r5.br() << '\n';
+//	std::cout << "r6's size : " << r6.size() << '\n'; 
+//
+//	Rect_<int> r7 = r5 & r6; //r5와 r6의 교차영역을 구함
+//	std::cout << "r7's topleft and bottomright : " << r7.tl() << ", " << r7.br() << '\n';
+//	std::cout << "r7's size : " << r7.size() << '\n';
+//
+//	Rect_<int> r8 = r5 | r6; //r5와 r6의 합영역을 구함
+//	std::cout << "r8's topleft and bottomright : " << r8.tl() << ", " << r8.br() << '\n';
+//	std::cout << "r8's size : " << r8.size() << '\n';
+//
+//	std::cout << "r8 contains Point(6,7)? : " << r8.contains(Point_<int>(6, 7)) << '\n'; //해당 point가 rect안에 포함되는지를 검사 
+//
+//	Rotated Rect_ 클래스 --> 회전된 사각형 클래스
+//	중심좌표, size, angle로 사각형 조작
+//
+//	RotatedRect rr1(Point2f(4.0, 5.0), Size2f(5.0, 6.0), 60); //(4.0, 5.0) 좌표가 중심인 크기가 5 x 6인 직사각형을 60도 회전시킴
+//	RotatedRect rr2(Point2f(0.0, 0.0), Point2f(1.0, 0.0), Point2f(1.0, 2.0)); //세 좌표로 직사각형 설정 --> 세좌표가 직사각형이여아함
+//
+//	std::cout << "rr2's center, size, angle : " << rr2.center << ", " << rr2.size << ", " << rr2.angle << '\n';
+//
+//	Point2f pts[4];
+//	rr2.points(pts); //pts에 rr2의 좌표 값 저장
+//	std::cout << "rr2's points" << '\n';
+//
+//	for (Point2f p : pts) {
+//		std::cout << p << '\n';
+//	}
+//
+//	std::cout << rr1.boundingRect() << '\n'; //rr1를 포함하면서 최소 크기인 직사각형을 return
+//
+//	Vec 클래스 --> 원소 수가 작을 때 사용하는 숫자 벡터 클래스
+//	Vec<int, 4> v1(1,2,3,4);
+//	std::cout << "v1 : " << v1 << '\n';
+//
+//	Vec2f v2(3.1, 4.1);
+//	std::cout << "v2 : " << v2 << '\n';
+//
+//	Point2f temp_p(v2); // vector --> point로 변환 가능
+//	std::cout << "temp_p : " << temp_p << '\n';
+//
+//	Scalar 클래스 --> Vec<typename, 4>, 화소 값을 저장할때 많이 사용, Scaler_<double>의 약자.
+//	Scalar_<float> bgr_t(0, 0, 255, 1);
+//	std::cout << "bgr_t : " << bgr_t << '\n';
+//	
+//	Scalar gray_scale = 210; // 첫번째 원소값만 설정되고 나머지는 다 0으로 저장
+//	std::cout << "gray_scale : " << gray_scale << '\n';
+//
+//	Mat mat_grayScale(Size(300, 300), CV_8UC1, Scalar(50, 0, 0, 0)); //grayscale
+//	Mat mat_trueColor(Size(300, 300), CV_8UC3, Scalar(50, 60, 100, 0)); //truecolor
+//
+//	imshow("GrayScale", mat_grayScale);
+//	waitKey();
+//	imshow("TrureScale", mat_trueColor);
+//	waitKey();
+//
+//	Range 클래스 --> 시퀀스에서 연속되는 서브 시퀀스를 지정하는 클래스
+//	Range r(1, 4); // 1, 2, 3 --> end는 포함하지 않음.
+//	std::cout << "Range r : " << r << '\n';
+//	std::cout << "Range r's size : " << r.size() << '\n';
+//	std::cout << "Range r is empty ?  : " << r.empty() << '\n';
+//}
